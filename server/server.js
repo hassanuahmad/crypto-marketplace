e30f0c4acc0f8683ec3038d56cba6525fc5d65e4
Jagraj Aulakh <aulakhjagraj@gmail.com>
2022-03-26 18:41:46 -0400

    deploy test

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const mysql = require("mysql2");
 const cors = require("cors");
+const bodyParser = require('body-parser');
 require("dotenv").config();
 
 app.use(cors());
