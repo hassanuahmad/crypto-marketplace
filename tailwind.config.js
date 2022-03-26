0ece6b5a14e9a213147d26ebc2e525d5c4717b9f
Jagraj Aulakh <aulakhjagraj@gmail.com>
2022-03-25 20:09:55 -0400

    Added tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				"cmp-primary": "#3E9B7B",
+				// "cmp-primary-dark": "#3E9B7B",
+				"cmp-black": "#04111D",
+				"cmp-light-gray": "#8C8CA1",
+			},
+			fontFamily: {
+				poppins: "'Poppins', sans-serif",
+			},
+		},
+	},
+	plugins: [],
+};
