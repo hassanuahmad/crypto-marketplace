7510e8bfe1d3f3a593f6b89b60b281a449df0cd1
ahmad12p@uwindsor.ca <ahmad12p@uwindsor.ca>
2022-03-26 15:57:17 -0400

    added check on profile page to see conected or not

--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -1,9 +1,19 @@
 import Profile from "../components/Profile Page/Profile";
+import { useContext } from "react";
+import WalletContext from "../contexts/WalletContext";
 
 const ProfilePage = () => {
+	const { isWalletConnected } = useContext(WalletContext);
+
 	return (
 		<div>
-			<Profile />
+			{isWalletConnected ? (
+				<Profile />
+			) : (
+				<div className="h-40 title3-text text-center py-16">
+					Connect Wallet to see your profile!
+				</div>
+			)}
 		</div>
 	);
 };
