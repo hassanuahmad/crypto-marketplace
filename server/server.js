dd4b5eb66f91bdecabe8bbe3061108d3d8b6de89
Jagraj Aulakh <aulakhjagraj@gmail.com>
2022-03-26 20:13:31 -0400

    Kicking CORS issues since 2001 :cool:

--- server/server.js
+++ server/server.js
@@ -3,11 +3,18 @@ const express = require("express");
 const app = express();
 const mysql = require("mysql2");
 const cors = require("cors");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 require("dotenv").config();
 
-app.use(cors());
 app.use(express.json());
+app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	next();
+});
 
 // DATABASE SETUP
 const db = mysql.createPool({
