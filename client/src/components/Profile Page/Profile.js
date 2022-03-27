d233eb354e3bf28fc7ba1e4b0be8802087e1a94a
ahmad12p@uwindsor.ca <ahmad12p@uwindsor.ca>
2022-03-27 00:38:51 -0400

    added formik to email in profile page

--- client/src/components/Profile Page/Profile.js
+++ client/src/components/Profile Page/Profile.js
@@ -1,15 +1,32 @@
 import React, { useContext } from "react";
 import Jazzicon from "react-jazzicon";
 import WalletContext from "../../contexts/WalletContext";
+import Button from "../../components/Button";
+import { faCircleArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import Axios from "axios";
 
 const Profile = () => {
-	const { accountAddress, userBalance } = useContext(WalletContext);
+	const { accountAddress, userBalance, formatMobileWalletAddress } =
+		useContext(WalletContext);
 
-	const formatMobileWalletAddress = () => {
-		return `${accountAddress.substring(0, 7)}...${accountAddress.substring(
-			accountAddress.length - 5
-		)}`;
-	};
+	const formik = useFormik({
+		initialValues: {
+			email: "",
+		},
+		validationSchema: Yup.object({
+			email: Yup.string().email("Invalid email address").required(),
+		}),
+
+		onSubmit: (values) => {
+			Axios.post("", {
+				
+			})
+			console.log(values);
+		},
+	});
 
 	return (
 		<div className="py-16">
@@ -29,16 +46,32 @@ const Profile = () => {
 					<span className="boldBody-text">{userBalance} ETH</span>
 				</p>
 				<div className="pt-10 flex flex-col">
-					<label className="pb-2" htmlFor="">
-						Email Address
-					</label>
-					<input
-						id="email"
-						name="email"
-						type="email"
-						placeholder="Email"
-						className="input-style"
-					/>
+					<form onSubmit={formik.handleSubmit} className="flex">
+						<input
+							id="email"
+							name="email"
+							type="email"
+							placeholder="Email"
+							className="input-style rounded-none rounded-l-lg"
+							onChange={formik.handleChange}
+							onBlur={formik.handleBlur}
+							value={formik.values.email}
+						/>
+
+						<Button
+							type="submit"
+							className="shadow-none rounded-none rounded-r-lg"
+							text={
+								<FontAwesomeIcon
+									icon={faCircleArrowRight}
+									className="text-2xl shadow-none"
+								/>
+							}
+						/>
+					</form>
+					{formik.touched.email && formik.errors.email ? (
+						<p className="form-error">{formik.errors.email}</p>
+					) : null}
 				</div>
 			</div>
 		</div>
